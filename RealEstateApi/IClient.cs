4a4ff201012ce4efa245ff9a996cc10ccbcab720
Sarmkadan <rutova2@gmail.com>
2021-03-16 10:46:09 +0200

    Добавьте файлы проекта.

---
+++ RealEstateApi/IClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml.Schema;
+using System.Xml;
+
+namespace RealEstateApi
+{
+    [ServiceContract]
+    public interface IClient
+    {
+        [OperationContract]
+        [WebGet]
+        XmlElement GetClients(string fio);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        IEnumerable<string> GetClientsJson(string value);
+    }
+}
